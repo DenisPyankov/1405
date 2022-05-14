9477cab4f196ab36828adadf65d7a0b93a53dff2
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-05-14 10:37:21 +0500

    Add files via upload

---
+++ 447.cpp
@@ -0,0 +1,23 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+    long long n, f = 1;
+    in >> n;
+    for (int i = 1; i <= n; ++i) {
+        f *= i;
+        while (f % 10 == 0) {
+            f /= 10;
+        }
+        f %= 1000000;
+    }
+    out << f % 10;
+
+
+
+}
