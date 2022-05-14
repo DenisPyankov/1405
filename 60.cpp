ed262acb0391840f8080247ebba6707f7361856f
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-05-14 08:57:59 +0500

    Add files via upload

---
+++ 60.cpp
@@ -0,0 +1,40 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	int n;
+	cin >> n;
+	int num = 1, n1 = 0, n2 = 0, b = 0;
+	while (b != 1) {
+		num++;
+		n1++;
+		int k = 0;
+		for (int i = 2; i <= num / 2; i++)
+			if (num % i == 0)
+				k++;
+		cout << num << " " << n1 << " " << k << endl;
+		if (k == 0 && n1 != 1) {
+			int k2= 0;
+			for (int i = 2; i <= n1 / 2; i++)
+				if (n1 % i == 0)
+					k2++;
+			if (k2 == 0)
+				n2++;
+			if (n2 == n)
+				b++;
+
+		}
+		
+	}
+
+
+	cout << num;
+
+
+
+}
