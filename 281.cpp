49692deb5595ccce25928fe2489ab916c4ab15e6
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-05-14 10:21:46 +0500

    Add files via upload

---
+++ 281.cpp
@@ -0,0 +1,26 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	int n, m;
+	cin >> n >> m;
+	vector <int> v;
+	v.push_back(1);
+	int a = 1, b = 1;
+	while (a != n + 1) {
+		b *= a;
+		v.push_back(b);
+		a++;
+	}
+	int x = 0;
+	for (int i = m; i <= n; ++i) {
+		x += v[n] / (v[i] * v[n - i]);
+	}
+	cout << x;
+	
+}
