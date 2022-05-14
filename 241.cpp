30001676b572c6a6b8c4d1c552e43e3a7c168ab4
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-05-14 09:40:18 +0500

    Add files via upload

---
+++ 241.cpp
@@ -0,0 +1,18 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	int n;
+	in >> n;
+	int a = n / 5 * 2 + 2;
+	int ans = a + n;
+	if (ans == 53 || ans == 66) {
+		ans++;
+	}
+	out << ans;
+}
